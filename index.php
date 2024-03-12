bddf18adea72b32c2bea3052cab169e970372259
aghabalaguluzade <agabala.oyunda@gmail.com>
2024-03-13 02:37:41 +0400

    simple router

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php
 
-$heading = "Home";
+require 'functions.php';
+// require 'router.php';
 
-require "views/index.view.php";
+// $dns = "mysql:host=localhost;port=3306;dbname=php-mvc-framework;charset=utf8mb4";
+// $pdo = new PDO($dns,'root',"");
+
+$dns = "mysql:host=localhost;port=3306;dbname=php-mvc-framework;charset=utf8mb4";
+$username = "root";
+$password = "";
+try {
+    $pdo = new PDO($dns, $username, $password);
+} catch (PDOException $e) {
+    echo "Bağlantı hatası: " . $e->getMessage();
+}
+
+
+
+
+// $statement = $pdo->prepare("SELECT * FROM posts");
+// $statement->execute();
+// $posts = $statement->fetchAll();
+// dd($posts);
---
+++ router.php
@@ -0,0 +1,27 @@
+<?php
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    'about' => 'controllers/about.php',
+    'contact' => 'controllers/contact.php'
+];
+
+function abort(int $code = 404): void
+{
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+}
+
+function routeToController(string $uri, array $routes): void
+{
+    if(array_key_exists($uri,$routes)) {
+        require $routes[$uri];
+    }else {
+        abort();
+    }
+}
+
+routeToController($uri, $routes);
